c596aee0b252888bb56a0b7bb7511edf6a989282
JeudyV <46583912+JeudyV@users.noreply.github.com>
2020-07-13 15:53:57 -0600

    add requirements

--- development_info_ebay.py
+++ development_info_ebay.py
@@ -4,6 +4,14 @@ import ebaysdk
 from ebaysdk import finding
 from ebaysdk.finding import Connection as finding
 from credentials import user, passw, host, port, database, appid
+import os
+
+user = os.getenv("DBUSER", 'root')
+passw = os.getenv("DBPASS", '1234')
+host = os.getenv("DBHOST", 'localhost')
+port = os.getenv("DBPORT", 3306)
+database = os.getenv("DBDATABASE", 'test_ebay_db') 
+appid = os.getenv("APPID", 'HaokunLi-KACards-PRD-f2eb84a53-383636d9') 
 
 mydb = create_engine('mysql+pymysql://' + user + ':' + passw + '@' + host + ':' + str(port) + '/' + database, echo=False)
 
@@ -14,10 +22,10 @@ def search_for_articles(year, company, see, player, db_name):
         'keywords': f'{year}, {company}, {see}, {player}',
     }
     )
-    
+
     dictstr = api.response.dict()
     df = pd.DataFrame(dictstr["searchResult"]["item"]).astype(str)
     df.to_sql(name=db_name, con=mydb, index=False, if_exists='replace')
 
 
-search_for_articles("2018/19", "Panini", "Prizm", "Kobe Bryant", "db_name3")
+search_for_articles("2018/19", "Panini", "Prizm", "Kobe Bryant", "db_name")
