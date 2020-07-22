faaec9d1e4f539e24bb00a9e9799f79e1bfbc90c
kervin5705 <kervin5705@gmail.com>
2020-07-22 14:08:15 -0600

    WIP

--- development_info_ebay.py
+++ development_info_ebay.py
@@ -3,14 +3,13 @@ from sqlalchemy import create_engine
 import ebaysdk
 from ebaysdk import finding
 from ebaysdk.finding import Connection as finding
-from credentials import user, passw, host, port, database, appid
 import os
 
 user = os.getenv("DBUSER", 'root')
 passw = os.getenv("DBPASS", '1234')
 host = os.getenv("DBHOST", 'localhost')
 port = os.getenv("DBPORT", 3306)
-database = os.getenv("DBDATABASE", 'test_ebay_db') 
+database = os.getenv("DBDATABASE", 'test_ebay_db_db_name') 
 appid = os.getenv("APPID", 'HaokunLi-KACards-PRD-f2eb84a53-383636d9') 
 
 mydb = create_engine('mysql+pymysql://' + user + ':' + passw + '@' + host + ':' + str(port) + '/' + database, echo=False)
@@ -24,8 +23,11 @@ def search_for_articles(year, company, see, player, db_name):
     )
 
     dictstr = api.response.dict()
+    print("1")
     df = pd.DataFrame(dictstr["searchResult"]["item"]).astype(str)
+    print("2")
     df.to_sql(name=db_name, con=mydb, index=False, if_exists='replace')
+    print("3")
 
 
-search_for_articles("2018/19", "Panini", "Prizm", "Kobe Bryant", "db_name")
+search_for_articles("2020", "Panini", "Prizm", "Kobe Bryant", "db_name")
